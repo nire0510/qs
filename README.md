9683a971c9bba6bd8a2628fe49a85cfc170d3a0a
Nir Elbaz <nir@nir-linux.Conduit-IL.com>
2014-12-30 14:26:01 +0200

    Change from constructor to function

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ QS helps you extract all query strings from a given or current url.
 ### Code samples:
 ```javascript
 // Extract **foo** query string key from a given url:
-new QS('http://www.somedomain.com/somepage?foo=bar').foo
+QS('http://www.somedomain.com/somepage?foo=bar').foo
 // => 'bar'
 
 // Check if **foo** query string key exists:
-new QS('http://www.somedomain.com/somepage?foo=bar').has('foo')
+QS('http://www.somedomain.com/somepage?foo=bar').has('foo')
 // => true
 ```
--- src/qs.js
+++ src/qs.js
@@ -1,45 +1,44 @@
 /**
  * @param {string} [strUrl] Url to parse. If null than current url is taken
- * @constructor
  */
 function QS (strUrl) {
-  this.url = strUrl || window.location.href;
-  this.qs = {};
+  var qs = {
+    /** @property  */
+    url: strUrl || window.location.href,
+    /** @property  */
+    tokens: {},
+    /**
+     * Checks if url contains specific query string
+     * @name log
+     */
+    has: function (strKey) {
+      return qs.tokens.hasOwnProperty(strKey);
+    },
+    /**
+     * Logs query strings dictionary
+     * @name log
+     */
+    log: function () {
+      console.log(qs.tokens);
+    }
+  }
 
-  // Extracts all tokens:
+  // Parse URL:
   function parse () {
     var re = /[?&](\w+)(?:=(\w+))?/g,
       match;
 
-    match = re.exec(this.url);
-    console.log(this.url);
+    match = re.exec(qs.url);
     while (match !== null) {
       // Add to global qs object:
-      this.qs[match[1]] = match[2] || null;
+      qs[match[1]] = match[2] || null;
       // Register qs keys as object's properties:
-      this[match[1]] = match[2] || null;
-      match = re.exec(this.url);
+      qs.tokens[match[1]] = match[2] || null;
+      match = re.exec(qs.url);
     }
-  }
+  };
 
-  // Parse URL with current object context:
-  parse.call(this);
+  parse();
 
-  return this;
-}
-
-/**
- * Logs query strings dictionary
- * @name log
- */
-QS.prototype.log = function () {
-  console.log(this.qs);
-}
-
-/**
- * Checks if url contains specific query string
- * @name log
- */
-QS.prototype.has = function (strKey) {
-  return this.qs.hasOwnProperty(strKey);
+  return qs;
 }
